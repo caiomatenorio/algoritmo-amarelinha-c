c92a48fd04176dd8bcb11947f3001a9d116d824e
Caio de Araújo <caiotdearaujo@gmail.com>
2024-03-02 19:51:22 -0300

    function prototype added

--- main.c
+++ main.c
@@ -1,57 +1,17 @@
 #include <stdio.h>
 
-int arrayLength(int array[2])
-{
-  if (array[1] == -1)
-  {
-    return 1;
-  }
-  else
-  {
-    return 2;
-  }
-}
-
-void printLocal(int local[2])
-{
-  if (local[0] == 0 && local[1] == -1)
-  {
-    printf("Você está no início da amarelinha\n");
-  }
-  else if (local[0] == 10)
-  {
-    printf("Você está no céu\nVocê se virou\n");
-  }
-  else if (arrayLength(local) == 2)
-  {
-    printf("Você pisou com dois pés em %i e %i\n", local[0], local[1]);
-  }
-  else
-  {
-    printf("Você pisou com um pé em %i\n", local[0]);
-  }
-}
-
-int isIn(int array[2], int value)
-{
-  for (int i = 0; i < arrayLength(array); i++)
-  {
-    if (array[i] == value)
-    {
-      return 1;
-    }
-  }
-  return 0;
-}
+int arrayLength(int[]);
+void printLocal(int[]);
+int isIn(int[], int);
 
 int main(void)
 {
   int pedraEm;
   int proximaCasa[2];
-  int amarelinha[9][2] = {{0, -1}, {1, -1}, {2, -1}, {3, 4}, {5, -1}, {6, -1}, {7, 8}, {9, -1}, {10, -1}};
+  int amarelinha[][2] = {{0, -1}, {1, -1}, {2, -1}, {3, 4}, {5, -1}, {6, -1}, {7, 8}, {9, -1}, {10, -1}};
   int rodada = 1;
   int indice = 3;
-  int pesEm[2] = {amarelinha[indice][0], amarelinha[indice][1]};
+  int pesEm[] = {amarelinha[indice][0], amarelinha[indice][1]};
 
   while (rodada < 10)
   {
@@ -137,4 +97,48 @@ int main(void)
   }
 
   printf("Você venceu a amarelinha\n");
+}
+
+int arrayLength(int array[2])
+{
+  if (array[1] == -1)
+  {
+    return 1;
+  }
+  else
+  {
+    return 2;
+  }
+}
+
+void printLocal(int local[2])
+{
+  if (local[0] == 0 && local[1] == -1)
+  {
+    printf("Você está no início da amarelinha\n");
+  }
+  else if (local[0] == 10)
+  {
+    printf("Você está no céu\nVocê se virou\n");
+  }
+  else if (arrayLength(local) == 2)
+  {
+    printf("Você pisou com dois pés em %i e %i\n", local[0], local[1]);
+  }
+  else
+  {
+    printf("Você pisou com um pé em %i\n", local[0]);
+  }
+}
+
+int isIn(int array[2], int value)
+{
+  for (int i = 0; i < arrayLength(array); i++)
+  {
+    if (array[i] == value)
+    {
+      return 1;
+    }
+  }
+  return 0;
 }
